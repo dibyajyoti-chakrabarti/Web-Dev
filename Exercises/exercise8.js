d7354463243901bff9861987dcb3d21ce551341b
Dibyajyoti Chakrabarti <dibyajyoti.chakrabarti.122@gmail.com>
2025-02-20 00:49:48 +0530

    Changes to Exercise 8

--- Exercises/exercise8.js
+++ Exercises/exercise8.js
@@ -9,15 +9,23 @@ function randomDelay(){
 }
 async function f(message){
     let a = await delay(randomDelay())
+    let body = document.body
+    let container = document.createElement("div")
+    Object.assign(container.style, {
+        backgroundColor: "black"
+    })
+    body.append(container)
+    container.innerHTML = message
+    
     console.log(message)
 }
 
 async function main() {
-  await f("Initializing Hacking");
-  await f("Reading your files");
-  await f("Password files Detected");
-  await f("Sending all password and personal files to server");
-  await f("Cleaning up");
+    await f("Initializing Hacking".toUpperCase());
+    await f("Reading your files".toUpperCase());
+    await f("Password files Detected".toUpperCase());
+    await f("Sending all password and personal files to server".toUpperCase());
+    await f("Cleaning up".toUpperCase());
 }
 
 main()
